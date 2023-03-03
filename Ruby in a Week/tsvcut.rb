ad55e73d84357d50ca1988c424b91be7f5698ae9
jack gleeson <jack@jpgleeson.com>
2023-03-02 16:16:08 -0800

    buffer the output per line.

--- Ruby in a Week/tsvcut.rb
+++ Ruby in a Week/tsvcut.rb
@@ -5,13 +5,16 @@ filter = "FirstName"
 while !ARGF.eof?
     line = ARGF.readline
     fields = line.split("\t")
+    output = ""
+
     if fields.length % 2 != 0
         puts "Bad input on #{line}. We need headers in the input lines"
         break
     end
     fields.each_slice(2) do |key, value|
         if key == filter
-            puts "#{key} - #{value}"
+            output += key + "\t" + value
         end
     end
+    puts output
 end
