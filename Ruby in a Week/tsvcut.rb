3119bec5d83a6d7a723441cd6877acacdcd88542
jack gleeson <jack@jpgleeson.com>
2023-03-02 16:14:55 -0800

    Only output filtered key field on stream

--- Ruby in a Week/tsvcut.rb
+++ Ruby in a Week/tsvcut.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 
+filter = "FirstName"
+
 while !ARGF.eof?
     line = ARGF.readline
     fields = line.split("\t")
     if fields.length % 2 != 0
-        puts "Bad input on #{line}"
+        puts "Bad input on #{line}. We need headers in the input lines"
         break
     end
-    fields.each do |field|
-        puts field
+    fields.each_slice(2) do |key, value|
+        if key == filter
+            puts "#{key} - #{value}"
+        end
     end
 end
